7184b461e6f9f398befb1b9189fa08b7bce8b256
jakub.coufal <jakub.coufal@wandera.com>
2018-03-13 11:20:38 +0100

    Refactored init command into two distinct get commands

---
+++ config/cmd/get.go
@@ -0,0 +1,118 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"github.com/wanderaorg/scccmd/config/client"
+)
+
+var (
+	source       string
+	application  string
+	profile      string
+	label        string
+	format       string
+	destination  string
+	fileMappings FileMappings
+)
+
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get the config from the given config server",
+}
+
+var getValuesCmd = &cobra.Command{
+	Use:   "values",
+	Short: "Get the config values in specified format from the given config server",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return executeGetValues(args)
+	},
+}
+
+var getFilesCmd = &cobra.Command{
+	Use:   "files",
+	Short: "Get the config files from the given config server",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return executeGetFiles(args)
+	},
+}
+
+func executeGetValues(args []string) error {
+	ext, err := client.ParseExtension(format)
+
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.
+		NewClient(client.Config{URI: source, Profile: profile, Application: application, Label: label}).
+		FetchAs(ext)
+
+	if err != nil {
+		return err
+	}
+
+	if destination != "" {
+		if Verbose {
+			fmt.Println("Config server response:")
+			fmt.Println(resp)
+		}
+
+		if err = ioutil.WriteFile(destination, []byte(resp), 0644); err != nil {
+			return err
+		}
+
+		if Verbose {
+			fmt.Println("Response written to: ", destination)
+		}
+	} else {
+		fmt.Print(resp)
+	}
+
+
+	return nil
+}
+
+func executeGetFiles(args []string) error {
+	for _, mapping := range fileMappings.Mappings() {
+		resp, err := client.
+			NewClient(client.Config{URI: source, Profile: profile, Application: application, Label: label}).
+			FetchFile(mapping.source)
+
+		if err != nil {
+			return err
+		}
+
+		if Verbose {
+			fmt.Println("Config server response:")
+			fmt.Println(resp)
+		}
+
+		if err = ioutil.WriteFile(mapping.destination, resp, 0644); err != nil {
+			return err
+		}
+
+		if Verbose {
+			fmt.Println("Response written to: ", mapping.destination)
+		}
+	}
+	return nil
+}
+
+func init() {
+	getCmd.AddCommand(getFilesCmd)
+	getCmd.AddCommand(getValuesCmd)
+	getCmd.PersistentFlags().StringVarP(&source, "source", "s", "", "address of the config server")
+	getCmd.PersistentFlags().StringVarP(&application, "application", "a", "", "name of the application to get the config for")
+	getCmd.PersistentFlags().StringVarP(&profile, "profile", "p", "default", "configuration profile")
+	getCmd.PersistentFlags().StringVarP(&label, "label", "l", "master", "configuration label")
+	getCmd.MarkFlagRequired("source")
+	getCmd.MarkFlagRequired("application")
+
+	getFilesCmd.Flags().VarP(&fileMappings, "files", "f", "files to get in form of source:destination pairs, example '--files application.yaml:config.yaml'")
+	getFilesCmd.MarkFlagRequired("files")
+
+	getValuesCmd.Flags().StringVarP(&format, "format", "f", "yaml", "output format might be one of 'json|yaml|properties'")
+	getValuesCmd.Flags().StringVarP(&destination, "destination", "d", "", "destination file name")
+}
--- config/cmd/get_test.go
+++ config/cmd/get_test.go
@@ -11,13 +11,13 @@ import (
 )
 
 func TestNoArgExecute(t *testing.T) {
-	err := executeInit(nil)
+	err := executeGetFiles(nil)
 	if err != nil {
 		t.Error("Execute failed with: ", err)
 	}
 }
 
-func TestExecute(t *testing.T) {
+func TestExecuteGetFiles(t *testing.T) {
 	var testParams = []struct {
 		testContent  string
 		appName      string
@@ -68,7 +68,70 @@ func TestExecute(t *testing.T) {
 			fileMappings = FileMappings{mappings: make([]FileMapping, 1)}
 			fileMappings.mappings[0] = FileMapping{source: tp.srcFileName, destination: tp.destFileName}
 
-			if err := executeInit(nil); err != nil {
+			if err := executeGetFiles(nil); err != nil {
+				t.Error("Execute failed with: ", err)
+			}
+
+			raw, err := ioutil.ReadFile(tp.destFileName)
+			defer os.Remove(tp.destFileName)
+			if err != nil {
+				t.Error("Expected to download file: ", err)
+			}
+
+			if response := strings.TrimRight(string(raw[:]), "\n"); response != tp.testContent {
+				t.Error(fmt.Sprintf("Expected response: '%s' got '%s' instead.", tp.testContent, response))
+			}
+		}()
+	}
+}
+
+func TestExecuteGetValues(t *testing.T) {
+	var testParams = []struct {
+		testContent  string
+		appName      string
+		profile      string
+		label        string
+		destFileName string
+		requestURI   string
+	}{
+		{"{\"foo\":\"bar\"}",
+			"app",
+			"default",
+			"master",
+			"dest",
+			"/master/app-default.yml"},
+		{"{\"bar\":\"foo\"}",
+			"app2",
+			"default",
+			"master",
+			"destination",
+			"/master/app2-default.yml"},
+		{"{\"foo\":\"bar\"}",
+			"app",
+			"prod",
+			"1.0.0",
+			"config.yaml",
+			"/1.0.0/app-prod.yml"},
+	}
+
+	for _, tp := range testParams {
+		func() {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.RequestURI != tp.requestURI {
+					t.Error(fmt.Sprintf("Expected call to '%s' but got '%s' instead.", tp.requestURI, r.RequestURI))
+				}
+
+				fmt.Fprintln(w, tp.testContent)
+			}))
+			defer ts.Close()
+
+			application = tp.appName
+			profile = tp.profile
+			label = tp.label
+			source = ts.URL
+			destination = tp.destFileName
+
+			if err := executeGetValues(nil); err != nil {
 				t.Error("Execute failed with: ", err)
 			}
 

e49f2baefaf460ebe676768c54c802af20a19c3e
jakub.coufal <jakub.coufal@wandera.com>
2018-03-15 19:35:45 +0100

    Added Encrypt/Decrypt into the client

--- config/client/client_test.go
+++ config/client/client_test.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -32,6 +33,41 @@ func TestNewClient(t *testing.T) {
 	assertString(t, "Incorrect Application", tp.application, c.Config().Application)
 	assertString(t, "Incorrect Profile", tp.profile, c.Config().Profile)
 	assertString(t, "Incorrect Label", tp.label, c.Config().Label)
+}
+
+func TestErrorResponse(t *testing.T) {
+	var tp = struct {
+		application string
+		profile     string
+		label       string
+		URI         string
+		testContent string
+		fileName    string
+	}{
+		"service",
+		"profile",
+		"master",
+		"/service/profile/master/File",
+		"test",
+		"File",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertString(t, "Incorrect URI call", tp.URI, r.RequestURI)
+		w.WriteHeader(406)
+	}))
+	defer ts.Close()
+
+	_, err := NewClient(Config{
+		URI:         ts.URL,
+		Application: tp.application,
+		Profile:     tp.profile,
+		Label:       tp.label,
+	}).FetchFile(tp.fileName)
+
+	if err == nil {
+		t.Error("FetchFile should have failed")
+	}
 }
 
 func TestClient_FetchFile(t *testing.T) {
@@ -103,6 +139,70 @@ func TestClient_FetchAsYAML(t *testing.T) {
 
 	if err != nil {
 		t.Error("FetchFile failed with: ", err)
+	}
+
+	assertString(t, "Content mismatch", tp.testContent, cont)
+}
+
+func TestClient_Encrypt(t *testing.T) {
+	var tp = struct {
+		URI         string
+		testContent string
+	}{
+		"/encrypt",
+		"test",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertString(t, "Incorrect Method", "POST", r.Method)
+		assertString(t, "Incorrect URI call", tp.URI, r.RequestURI)
+
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+
+		assertString(t, "Incorrect Content received", tp.testContent, buf.String())
+		fmt.Fprintln(w, tp.testContent)
+	}))
+	defer ts.Close()
+
+	cont, err := NewClient(Config{
+		URI: ts.URL,
+	}).Encrypt(tp.testContent)
+
+	if err != nil {
+		t.Error("Encrypt failed with: ", err)
+	}
+
+	assertString(t, "Content mismatch", tp.testContent, cont)
+}
+
+func TestClient_Decrypt(t *testing.T) {
+	var tp = struct {
+		URI         string
+		testContent string
+	}{
+		"/decrypt",
+		"test",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertString(t, "Incorrect Method", "POST", r.Method)
+		assertString(t, "Incorrect URI call", tp.URI, r.RequestURI)
+
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+
+		assertString(t, "Incorrect Content received", tp.testContent, buf.String())
+		fmt.Fprintln(w, tp.testContent)
+	}))
+	defer ts.Close()
+
+	cont, err := NewClient(Config{
+		URI: ts.URL,
+	}).Decrypt(tp.testContent)
+
+	if err != nil {
+		t.Error("Decrypt failed with: ", err)
 	}
 
 	assertString(t, "Content mismatch", tp.testContent, cont)

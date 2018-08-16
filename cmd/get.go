dd383fb4a632de629b21d1bb3170dc54220c1358
Jiri Sedlacek <jiri.sedlacek@wandera.com>
2018-08-16 09:04:32 +0200

    Get files command improved to be able to output to stdout.

--- cmd/get.go
+++ cmd/get.go
@@ -6,6 +6,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"io/ioutil"
+	"os"
 )
 
 var gp = struct {
@@ -86,11 +87,17 @@ func ExecuteGetFiles(args []string) error {
 		log.Debug("Config server response:")
 		log.Debug(string(resp))
 
-		if err = ioutil.WriteFile(mapping.destination, resp, 0644); err != nil {
-			return err
-		}
+		if mapping.destination == "-" {
+			os.Stdout.Write(resp)
+			fmt.Println()
+			log.Debug("Response written to stdout")
+		} else {
+			if err = ioutil.WriteFile(mapping.destination, resp, 0644); err != nil {
+				return err
+			}
 
-		log.Debug("Response written to: ", mapping.destination)
+			log.Debug("Response written to: ", mapping.destination)
+		}
 	}
 	return nil
 }
@@ -105,7 +112,7 @@ func init() {
 	getCmd.MarkFlagRequired("source")
 	getCmd.MarkFlagRequired("application")
 
-	getFilesCmd.Flags().VarP(&gp.fileMappings, "files", "f", "files to get in form of source:destination pairs, example '--files application.yaml:config.yaml'")
+	getFilesCmd.Flags().VarP(&gp.fileMappings, "files", "f", "files to get in form of source:destination pairs, you can use - as a output to stdout, example '--files application.yaml:config.yaml'")
 	getFilesCmd.MarkFlagRequired("files")
 
 	getValuesCmd.Flags().StringVarP(&gp.format, "format", "f", "yaml", "output format might be one of 'json|yaml|properties'")
